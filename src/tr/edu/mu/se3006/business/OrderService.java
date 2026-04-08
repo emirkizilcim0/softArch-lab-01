3a414b7236871dab27e9d8d10ef672e8e6ad2c6b
emirkizilcim0 <emirkzlcm0@gmail.com>
2026-04-08 10:39:17 +0300

    lab 01

---
+++ src/tr/edu/mu/se3006/business/OrderService.java
@@ -0,0 +1,26 @@
+package tr.edu.mu.se3006.business;
+import tr.edu.mu.se3006.domain.Product;
+import tr.edu.mu.se3006.persistence.ProductRepository;
+
+public class OrderService {
+    // TODO: Define ProductRepository dependency
+    public ProductRepository productRepository;
+    
+    // TODO: Implement Constructor Injection
+    public OrderService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public void placeOrder(Long productId, int quantity) {
+        // TODO 1: Find product via repository
+        Product product = productRepository.findById(productId);
+        // TODO 2: Check stock (throw IllegalArgumentException if insufficient)
+        if (product.getStock() < quantity) {
+            throw new IllegalArgumentException("Insufficient stock for product: " + product.getName());
+        }
+        // TODO 3: Reduce stock
+        product.setStock(product.getStock() - quantity);
+        // TODO 4: Save updated product
+        productRepository.save(product);
+    }
+}
